7842869649ee6d814c4181b3556f3151b20c1281
AN23702 <161464637+AN23702@users.noreply.github.com>
2024-08-20 23:01:07 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,3 +134,12 @@ document.addEventListener('click', (event) => {
     subMenuLv1.style.display = 'none';
   }
 });
+
+// back to top bar
+window.onscroll = function() {
+    if (window.pageYOffset > 0) {
+      document.querySelector('.back-to-top-bar').style.right = '20px';
+    } else {
+      document.querySelector('.back-to-top-bar').style.right = '-9999px';
+    }
+  };
